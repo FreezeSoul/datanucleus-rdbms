6f5f1840c101d39108ec05644d27279fef78077c
andyjefferson <andy@datanucleus.org>
2026-05-06 14:43:25 +0100

    Add fix for MariaDB detection

--- src/main/java/org/datanucleus/store/rdbms/adapter/MySQLAdapter.java
+++ src/main/java/org/datanucleus/store/rdbms/adapter/MySQLAdapter.java
@@ -97,7 +97,7 @@ public class MySQLAdapter extends BaseDatastoreAdapter
     {
         super(metadata);
 
-        if (driverName.equalsIgnoreCase("mariadb-jdbc"))
+        if (driverName.equalsIgnoreCase("mariadb-jdbc") || driverName.contains("MariaDB"))
         {
             isMariaDB = true;
         }
@@ -165,7 +165,7 @@ public class MySQLAdapter extends BaseDatastoreAdapter
         supportedOptions.add(OPERATOR_BITWISE_OR);
         supportedOptions.add(OPERATOR_BITWISE_XOR);
         supportedOptions.add(PARAMETER_IN_CASE_IN_UPDATE_CLAUSE);
-        supportedOptions.add(ORDER_BY_IN_UPDATE_STATEMENT);
+        supportedOptions.add(ORDER_BY_IN_UPDATE_STATEMENT); // TODO Does this apply to all version of MySQL, and also to MariaDB?
 
 //        supportedOptions.add(NATIVE_ENUM_TYPE); // There is no point to supporting this since "CHECK IN(...)" is ANSI standard and does the same
 
